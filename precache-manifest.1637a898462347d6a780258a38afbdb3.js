5a167d3c4f4b1cabf3cddd0eb33a7f3024ef77e6
samiraydi <samiraydi26@gmail.com>
2019-07-04 16:19:51 +0100

    Updates

---
+++ precache-manifest.1637a898462347d6a780258a38afbdb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb4ce706bb80744ae622ee856ddb19a0",
+    "url": "/loading/index.html"
+  },
+  {
+    "revision": "e1c337a9697cd37cce76",
+    "url": "/loading/static/css/main.6e083e96.chunk.css"
+  },
+  {
+    "revision": "655194bfc978ad93c8de",
+    "url": "/loading/static/js/2.ec8b0532.chunk.js"
+  },
+  {
+    "revision": "e1c337a9697cd37cce76",
+    "url": "/loading/static/js/main.891b3971.chunk.js"
+  },
+  {
+    "revision": "0d6fc0494f1feea05f83",
+    "url": "/loading/static/js/runtime~main.88bc64f9.js"
+  }
+]);
